a5ee6547d42fba2a78d71d5de1f98353a8a16539
kenseifunakoshianderson-lab <kensei.funakoshi.anderson@gmail.com>
2026-03-06 10:25:38 -0800

    Update player.js

--- engine/player.js
+++ engine/player.js
@@ -18,25 +18,81 @@ this.JUMP_HEIGHT=1.2522
 this.WALK=259/60
 this.RUN=336.72/60
 
+this.yaw=0
+this.pitch=0
+
 this.keys={}
 
 document.addEventListener("keydown",e=>this.keys[e.code]=true)
 document.addEventListener("keyup",e=>this.keys[e.code]=false)
 
+
+
+/* pointer lock */
+
+document.body.addEventListener("click",()=>{
+
+document.body.requestPointerLock()
+
+})
+
+
+
+document.addEventListener("mousemove",e=>{
+
+if(document.pointerLockElement!==document.body) return
+
+this.yaw -= e.movementX*0.002
+this.pitch -= e.movementY*0.002
+
+this.pitch=Math.max(-Math.PI/2,Math.min(Math.PI/2,this.pitch))
+
+})
+
 }
+
+
 
 update(){
 
 let speed=this.keys["ControlLeft"]?this.RUN:this.WALK
 
-if(this.keys["KeyW"])this.position.z-=speed*0.016
-if(this.keys["KeyS"])this.position.z+=speed*0.016
-if(this.keys["KeyA"])this.position.x-=speed*0.016
-if(this.keys["KeyD"])this.position.x+=speed*0.016
+let forward=new THREE.Vector3(
+Math.sin(this.yaw),
+0,
+Math.cos(this.yaw)
+)
 
-this.velY-=this.GRAVITY
+let right=new THREE.Vector3(
+forward.z,
+0,
+-forward.x
+)
 
-this.position.y+=this.velY
+
+
+if(this.keys["KeyW"])
+this.position.add(forward.clone().multiplyScalar(-speed*0.016))
+
+if(this.keys["KeyS"])
+this.position.add(forward.clone().multiplyScalar(speed*0.016))
+
+if(this.keys["KeyA"])
+this.position.add(right.clone().multiplyScalar(-speed*0.016))
+
+if(this.keys["KeyD"])
+this.position.add(right.clone().multiplyScalar(speed*0.016))
+
+
+
+/* gravity */
+
+this.velY -= this.GRAVITY
+this.position.y += this.velY
+
+
+
+/* ground check */
 
 let ground=this.world.getGround(this.position.x,this.position.z)
 
@@ -46,19 +102,36 @@ this.position.y=ground
 this.velY=0
 this.onGround=true
 
+}else{
+
+this.onGround=false
+
 }
 
-if(this.keys["Space"]&&this.onGround){
+
+
+/* jump */
+
+if(this.keys["Space"] && this.onGround){
 
 this.velY=Math.sqrt(2*this.GRAVITY*this.JUMP_HEIGHT)
 
 }
+
+
+
+/* camera */
 
 this.camera.position.set(
 this.position.x,
 this.position.y+1.6,
 this.position.z
 )
+
+this.camera.rotation.order="YXZ"
+
+this.camera.rotation.y=this.yaw
+this.camera.rotation.x=this.pitch
 
 }
 
