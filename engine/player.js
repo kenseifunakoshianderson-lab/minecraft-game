0e2133ca7649452c761ba52879990973bb7ea0c2
kenseifunakoshianderson-lab <kensei.funakoshi.anderson@gmail.com>
2026-03-06 10:29:08 -0800

    Update player.js

--- engine/player.js
+++ engine/player.js
@@ -7,16 +7,16 @@ constructor(camera,world){
 this.camera=camera
 this.world=world
 
-this.position=new THREE.Vector3(0,30,0)
+this.position=new THREE.Vector3(0,40,0)
 
 this.velY=0
 this.onGround=false
 
 this.GRAVITY=0.08
-this.JUMP_HEIGHT=1.2522
+this.JUMP=0.42
 
-this.WALK=259/60
-this.RUN=336.72/60
+this.walkSpeed=259/60
+this.runSpeed=336.72/60
 
 this.yaw=0
 this.pitch=0
@@ -27,8 +27,6 @@ document.addEventListener("keydown",e=>this.keys[e.code]=true)
 document.addEventListener("keyup",e=>this.keys[e.code]=false)
 
 
-
-/* pointer lock */
 
 document.body.addEventListener("click",()=>{
 
@@ -42,8 +40,8 @@ document.addEventListener("mousemove",e=>{
 
 if(document.pointerLockElement!==document.body) return
 
-this.yaw -= e.movementX*0.002
-this.pitch -= e.movementY*0.002
+this.yaw-=e.movementX*0.002
+this.pitch-=e.movementY*0.002
 
 this.pitch=Math.max(-Math.PI/2,Math.min(Math.PI/2,this.pitch))
 
@@ -55,7 +53,7 @@ this.pitch=Math.max(-Math.PI/2,Math.min(Math.PI/2,this.pitch))
 
 update(){
 
-let speed=this.keys["ControlLeft"]?this.RUN:this.WALK
+let speed=this.keys["ControlLeft"]?this.runSpeed:this.walkSpeed
 
 let forward=new THREE.Vector3(
 Math.sin(this.yaw),
@@ -85,14 +83,10 @@ this.position.add(right.clone().multiplyScalar(speed*0.016))
 
 
 
-/* gravity */
-
-this.velY -= this.GRAVITY
-this.position.y += this.velY
+this.velY-=this.GRAVITY
+this.position.y+=this.velY
 
 
-
-/* ground check */
 
 let ground=this.world.getGround(this.position.x,this.position.z)
 
@@ -110,17 +104,13 @@ this.onGround=false
 
 
 
-/* jump */
-
 if(this.keys["Space"] && this.onGround){
 
-this.velY=Math.sqrt(2*this.GRAVITY*this.JUMP_HEIGHT)
+this.velY=this.JUMP
 
 }
 
 
-
-/* camera */
 
 this.camera.position.set(
 this.position.x,
