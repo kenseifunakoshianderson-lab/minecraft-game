d8b79597ac7bd34f1ae808853229c22464e41a01
kenseifunakoshianderson-lab <kensei.funakoshi.anderson@gmail.com>
2026-03-06 11:10:56 -0800

    Update mesher.js

--- engine/mesher.js
+++ engine/mesher.js
@@ -4,7 +4,6 @@ import {BLOCK_COLORS} from "./blocks.js"
 export function buildMesh(blocks){
   const vertices=[]
   const colors=[]
-
   function has(x,y,z){ return blocks.has(x+","+y+","+z) }
 
   for(let key of blocks.keys()){
@@ -12,22 +11,14 @@ export function buildMesh(blocks){
     let type=blocks.get(key)
     let color=new THREE.Color(BLOCK_COLORS[type]||0xffffff)
 
-    // FRONT
     if(!has(x,y,z+1)) face([x,y,z+1],[x+1,y,z+1],[x+1,y+1,z+1],[x,y+1,z+1])
-    // BACK
     if(!has(x,y,z-1)) face([x+1,y,z],[x,y,z],[x,y+1,z],[x+1,y+1,z])
-    // LEFT
     if(!has(x-1,y,z)) face([x,y,z],[x,y,z+1],[x,y+1,z+1],[x,y+1,z])
-    // RIGHT
     if(!has(x+1,y,z)) face([x+1,y,z+1],[x+1,y,z],[x+1,y+1,z],[x+1,y+1,z+1])
-    // TOP
     if(!has(x,y+1,z)) face([x,y+1,z],[x,y+1,z+1],[x+1,y+1,z+1],[x+1,y+1,z])
-    // BOTTOM
     if(!has(x,y-1,z)) face([x,y,z],[x+1,y,z],[x+1,y,z+1],[x,y,z+1])
 
-    function face(a,b,c,d){
-      push(a,b,c); push(a,c,d)
-    }
+    function face(a,b,c,d){ push(a,b,c); push(a,c,d) }
     function push(a,b,c){
       vertices.push(...a,...b,...c)
       for(let i=0;i<3;i++) colors.push(color.r,color.g,color.b)
