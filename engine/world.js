c3892405c1d6beff8856c44078096efad84628e2
kenseifunakoshianderson-lab <kensei.funakoshi.anderson@gmail.com>
2026-03-06 10:02:20 -0800

    Update world.js

--- engine/world.js
+++ engine/world.js
@@ -1,1 +1,62 @@
+import {Chunk} from "./chunk.js"
 
+export class World{
+
+constructor(scene){
+
+this.scene=scene
+
+this.chunks=new Map()
+
+this.CHUNK=16
+
+}
+
+key(x,z){
+
+return x+","+z
+
+}
+
+update(playerPos){
+
+let cx=Math.floor(playerPos.x/this.CHUNK)
+let cz=Math.floor(playerPos.z/this.CHUNK)
+
+for(let x=-4;x<=4;x++)
+for(let z=-4;z<=4;z++){
+
+let k=this.key(cx+x,cz+z)
+
+if(!this.chunks.has(k)){
+
+let chunk=new Chunk(cx+x,cz+z,this.scene)
+
+this.chunks.set(k,chunk)
+
+}
+
+}
+
+}
+
+getGround(x,z){
+
+let gx=Math.floor(x)
+let gz=Math.floor(z)
+
+for(let y=256;y>-50;y--){
+
+for(let c of this.chunks.values()){
+
+if(c.hasBlock(gx,y,gz)) return y+1
+
+}
+
+}
+
+return -100
+
+}
+
+}
