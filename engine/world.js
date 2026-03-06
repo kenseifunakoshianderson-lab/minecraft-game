e6a204994760850b2a3ebbc1dce4baac44de0114
kenseifunakoshianderson-lab <kensei.funakoshi.anderson@gmail.com>
2026-03-06 12:34:00 -0800

    Update world.js

--- engine/world.js
+++ engine/world.js
@@ -18,7 +18,6 @@ export class World{
         let k = this.key(cx+x, cz+z);
         if(!this.chunks.has(k)){
           const blocks = new Map();
-          // simple flat terrain
           for(let bx=0;bx<16;bx++)
             for(let bz=0;bz<16;bz++)
               for(let by=0;by<10;by++){
