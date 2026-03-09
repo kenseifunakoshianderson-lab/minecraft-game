5ff621db3c28dbc6f90120241f5a27bf5c1da480
kenseifunakoshianderson-lab <kensei.funakoshi.anderson@gmail.com>
2026-03-09 14:37:18 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,32 +3,29 @@ import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160/build/three.mod
 import {World} from "./engine/world.js"
 import {Player} from "./engine/player.js"
 
-const scene=new THREE.Scene()
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0x87CEEB)
 
-scene.background=new THREE.Color(0x87CEEB)
-
-const camera=new THREE.PerspectiveCamera(
+const camera = new THREE.PerspectiveCamera(
 75,
 window.innerWidth/window.innerHeight,
 0.1,
 1000
 )
 
-const renderer=new THREE.WebGLRenderer()
-
+const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth,window.innerHeight)
 
 document.body.appendChild(renderer.domElement)
 
 scene.add(new THREE.AmbientLight(0xffffff,0.7))
 
-const sun=new THREE.DirectionalLight(0xffffff,1)
+const sun = new THREE.DirectionalLight(0xffffff,1)
 sun.position.set(100,200,100)
-
 scene.add(sun)
 
-const world=new World(scene)
-const player=new Player(camera,world)
+const world = new World(scene)
+const player = new Player(camera,world)
 
 function animate(){
 
