aa594e63cc47a79809624f19e26186cf23d826d5
kenseifunakoshianderson-lab <kensei.funakoshi.anderson@gmail.com>
2026-03-09 14:37:42 -0700

    Update world.js

--- engine/world.js
+++ engine/world.js
@@ -5,11 +5,11 @@ export class World{
 
 constructor(scene){
 
-this.scene=scene
-this.chunks=new Map()
+this.scene = scene
+this.chunks = new Map()
 
-this.CHUNK=16
-this.HEIGHT=32
+this.CHUNK = 16
+this.HEIGHT = 32
 
 }
 
@@ -19,29 +19,29 @@ return x+","+z
 
 generateChunk(cx,cz){
 
-const blocks=new Map()
+const blocks = new Map()
 
 for(let x=0;x<this.CHUNK;x++)
 for(let z=0;z<this.CHUNK;z++){
 
-const wx=cx*this.CHUNK+x
-const wz=cz*this.CHUNK+z
+const wx = cx*this.CHUNK+x
+const wz = cz*this.CHUNK+z
 
-const height=Math.floor(
-10+
-Math.sin(wx*0.05)*3+
+const height = Math.floor(
+10 +
+Math.sin(wx*0.05)*3 +
 Math.sin(wz*0.05)*3
 )
 
 for(let y=0;y<this.HEIGHT;y++){
 
-let type=BLOCKS.AIR
+let type = BLOCKS.AIR
 
 if(y<height){
 
-if(y===height-1) type=BLOCKS.GRASS
-else if(y>height-4) type=BLOCKS.DIRT
-else type=BLOCKS.STONE
+if(y===height-1) type = BLOCKS.GRASS
+else if(y>height-4) type = BLOCKS.DIRT
+else type = BLOCKS.STONE
 
 }
 
@@ -51,7 +51,7 @@ blocks.set(wx+","+y+","+wz,type)
 
 }
 
-const mesh=buildMesh(blocks)
+const mesh = buildMesh(blocks)
 
 this.scene.add(mesh)
 
@@ -67,15 +67,15 @@ mesh:mesh
 
 update(playerPos){
 
-const cx=Math.floor(playerPos.x/this.CHUNK)
-const cz=Math.floor(playerPos.z/this.CHUNK)
+const cx = Math.floor(playerPos.x/this.CHUNK)
+const cz = Math.floor(playerPos.z/this.CHUNK)
 
-const render=3
+const render = 3
 
 for(let x=-render;x<=render;x++)
 for(let z=-render;z<=render;z++){
 
-const k=this.key(cx+x,cz+z)
+const k = this.key(cx+x,cz+z)
 
 if(!this.chunks.has(k))
 this.generateChunk(cx+x,cz+z)
@@ -84,11 +84,11 @@ this.generateChunk(cx+x,cz+z)
 
 for(let k of this.chunks.keys()){
 
-const [x,z]=k.split(",").map(Number)
+const [x,z] = k.split(",").map(Number)
 
 if(Math.abs(x-cx)>render || Math.abs(z-cz)>render){
 
-const chunk=this.chunks.get(k)
+const chunk = this.chunks.get(k)
 
 this.scene.remove(chunk.mesh)
 
