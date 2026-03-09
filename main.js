286c31c5544d89839438c976f4bbbf7ab763a787
kenseifunakoshianderson-lab <kensei.funakoshi.anderson@gmail.com>
2026-03-09 14:15:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js"
+
+import {World} from "./engine/world.js"
+import {Player} from "./engine/player.js"
+
 const scene=new THREE.Scene()
 
 scene.background=new THREE.Color(0x87CEEB)
@@ -10,144 +15,28 @@ window.innerWidth/window.innerHeight,
 )
 
 const renderer=new THREE.WebGLRenderer()
+
 renderer.setSize(window.innerWidth,window.innerHeight)
+
 document.body.appendChild(renderer.domElement)
 
 scene.add(new THREE.AmbientLight(0xffffff,0.7))
 
 const sun=new THREE.DirectionalLight(0xffffff,1)
 sun.position.set(100,200,100)
+
 scene.add(sun)
 
-const loader=new THREE.TextureLoader()
-const atlas = loader.load("./textures/atlas.png")
-
-atlas.magFilter=THREE.NearestFilter
-atlas.minFilter=THREE.NearestFilter
-
-function block(x,y,z){
-
-const geo=new THREE.BoxGeometry(1,1,1)
-
-const mat=new THREE.MeshLambertMaterial({map:atlas})
-
-const mesh=new THREE.Mesh(geo,mat)
-
-mesh.position.set(x,y,z)
-
-scene.add(mesh)
-
-}
-
-function generate(){
-
-for(let x=-20;x<20;x++){
-
-for(let z=-20;z<20;z++){
-
-let h=Math.floor(
-10+Math.sin(x*0.4)+Math.sin(z*0.4)
-)
-
-for(let y=0;y<h;y++){
-
-block(x,y,z)
-
-}
-
-}
-
-}
-
-}
-
-generate()
-
-let yvel=0
-const gravity=-0.04
-const jump=0.28
-
-const keys={}
-
-document.addEventListener("keydown",e=>{
-
-keys[e.code]=true
-
-if(e.code=="KeyE"){
-
-const inv=document.getElementById("inventory")
-
-inv.style.display=
-inv.style.display=="none"?"grid":"none"
-
-}
-
-})
-
-document.addEventListener("keyup",e=>keys[e.code]=false)
-
-document.body.onclick=()=>document.body.requestPointerLock()
-
-let yaw=0
-let pitch=0
-
-document.addEventListener("mousemove",e=>{
-
-if(document.pointerLockElement!==document.body)return
-
-yaw-=e.movementX*0.002
-pitch-=e.movementY*0.002
-
-})
-
-camera.position.set(0,15,0)
+const world=new World(scene)
+const player=new Player(camera,world)
 
 function animate(){
 
 requestAnimationFrame(animate)
 
-const speed=0.15
+player.update()
 
-const forward=new THREE.Vector3(
-Math.sin(yaw),
-0,
-Math.cos(yaw)
-)
-
-const right=new THREE.Vector3(
-forward.z,
-0,
--forward.x
-)
-
-if(keys["KeyW"])
-camera.position.add(forward.clone().multiplyScalar(-speed))
-
-if(keys["KeyS"])
-camera.position.add(forward.clone().multiplyScalar(speed))
-
-if(keys["KeyA"])
-camera.position.add(right.clone().multiplyScalar(-speed))
-
-if(keys["KeyD"])
-camera.position.add(right.clone().multiplyScalar(speed))
-
-yvel+=gravity
-
-camera.position.y+=yvel
-
-if(camera.position.y<10){
-
-camera.position.y=10
-yvel=0
-
-if(keys["Space"]) yvel=jump
-
-}
-
-camera.rotation.order="YXZ"
-camera.rotation.y=yaw
-camera.rotation.x=pitch
+world.update(player.pos)
 
 renderer.render(scene,camera)
 
