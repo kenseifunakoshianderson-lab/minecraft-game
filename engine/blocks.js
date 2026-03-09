e36eb900bee57fd0583522e4c274231233a89ef6
kenseifunakoshianderson-lab <kensei.funakoshi.anderson@gmail.com>
2026-03-09 14:13:34 -0700

    Update blocks.js

--- engine/blocks.js
+++ engine/blocks.js
@@ -1,21 +1,28 @@
 export const BLOCKS = {
-
 AIR:0,
-
 GRASS:1,
-
 DIRT:2,
-
 STONE:3
-
 }
 
-export const BLOCK_COLORS = {
+export const UV = {
 
-1:0x55aa55,
+1:{ // grass
+top:[0,0],
+side:[1,0],
+bottom:[2,0]
+},
 
-2:0x8b4513,
+2:{ // dirt
+top:[2,0],
+side:[2,0],
+bottom:[2,0]
+},
 
-3:0x888888
+3:{ // stone
+top:[3,0],
+side:[3,0],
+bottom:[3,0]
+}
 
 }
